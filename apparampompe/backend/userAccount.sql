b2c4b8e17dc63f4c7b64a60b9c775ec5c2007ee2
Vaal34 <5619@holbertonstudents.com>
2024-06-05 15:58:49 +0200

    sql ddl

--- apparampompe/backend/userAccount.sql
+++ apparampompe/backend/userAccount.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `users_log` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `username` varchar(50) NOT NULL,
-    `password_hash` varchar(255) NOT NULL,
-    `email` varchar(100) NOT NULL,
-    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `id` CHAR(36) NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `password_hash` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY `username` (`username`),
     UNIQUE KEY `email` (`email`)
-) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
