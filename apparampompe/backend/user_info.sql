6bee070c3edc88f2a1946413eb5d1eb9e633d8ca
Vaal34 <5619@holbertonstudents.com>
2024-06-16 02:35:49 +0200

    user_info and user_stat table schemas and add foreign key constraint for gameName in user_stat table

--- apparampompe/backend/user_info.sql
+++ apparampompe/backend/user_info.sql
@@ -7,4 +7,14 @@ CREATE TABLE `user_info` (
     PRIMARY KEY (`id`),
     KEY `user_id` (`user_id`),
     CONSTRAINT `user_info_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_log` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'user info'
+) ENGINE = InnoDB AUTO_INCREMENT = 20 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'user info'
+
+ALTER TABLE user_info ADD UNIQUE KEY (current_target);
+ALTER TABLE user_stat 
+ADD CONSTRAINT user_stat_ibfk_2 
+FOREIGN KEY (gameName) 
+REFERENCES user_info (current_target) 
+ON DELETE CASCADE 
+ON UPDATE CASCADE;
+
+
--- apparampompe/backend/user_stat.sql
+++ apparampompe/backend/user_stat.sql
@@ -4,7 +4,9 @@ CREATE TABLE `user_stat` (
     `pompe` int NOT NULL DEFAULT '0' COMMENT 'pompe count',
     `calorie` int NOT NULL DEFAULT '0' COMMENT 'calorie count',
     `username` varchar(255) DEFAULT NULL,
+    `gameName` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `user_id` (`user_id`),
+    KEY `gameName` (`gameName`),
     CONSTRAINT `user_stat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'user stat info'
+) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'user stat info'
